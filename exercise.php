0de28f2471035864452433c2e9da0ac8385aa3f4
Ruan Lopes <121831380+rlopes16@users.noreply.github.com>
2024-06-30 21:13:27 -0300

    -adicionado ícone à aba do navegador;
    -rodapé atualizado c/ adição de LinkedIn, FUNÇÃO(<?= ... ?>) em php para ano atual e estilização para harmonia dos elementos;
    
    -nova pág .php para visualização de exercícios, e feito a estilização da mesma em css com botões de sem formação e para voltar à pág anterior s/ consultar histórico.

---
+++ exercise.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/ico/php.ico" type="image/x-icon"> <!-- icone da aba -->
+    <title> Exercício 🐘</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com"> <!-- importando fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/exercise.css">
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg" width="70px" />
+        <h2> PHP na prática 🎯 </h2> 
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde"> SEM FORMATAÇÃO </a>
+        <a href="index.php" class="vermelho"> Voltar </a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            
+        </div>
+
+    </main>
+
+    <footer class="rodape"> 
+        <a href="https://github.com/ruan-lopes16" target="_blank"> <!-- colocando github para acessar -->
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg" width="40px"/>
+        </a>
+        <p>RUAN S. LOPES &copy; <?= date('Y');?></p> 
+<!-- código php - "M" mês-extenso, "m" mês-número, "D" dia da semana(inglês), "d" dia-número, "Y" ano-completo, "y" ano-dois último digitos -->
+        <a href="https://www.linkedin.com/in/ruanslopes/" target="_blank"> <!--colocado linkedim para acessar -->
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg" width="40px"/>
+        </a>
+    </footer>
+ 
+</body>
+</html> 
---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/ico/php.ico" type="image/x-icon"> <!-- icone da aba -->
+    <title> PHP🐘 </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com"> <!-- importando fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+-+
+</head>
+<body>
+    <header class="cabecalho">
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg" width="70px" />
+        <!-- <h1>PHP 🐘</h1> -->
+        <h2>Índice dos exercícios</h2> 
+    </header>
+
+    <main class="principal">
+
+        <div class="conteudo">
+            <nav class="modulos">
+                <!-- CRIANDO MÓDULOS PARA OS EXERCÍCIOS -->
+                <div class="modulo verde">
+                    <h3>MÓDULO 01 - </h3>
+                    <ul> <!-- link para acesso ao exercício -->
+                        <li> <a href="exercise.php"> EXERCÍCIOS </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3> MÓDULO 02 - </h3>
+                    <ul>
+                        <li> <a href="#"> EXERCÍCIOS </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                    </ul>
+                </div>
+                <div class="modulo azul">
+                    <h3>MÓDULO 03 -</h3>
+                    <ul>
+                        <li> <a href="#">EXERCÍCIOS</a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                    </ul>
+                </div>
+                <div class="modulo roxo">
+                    <h3>MÓDULO 04 - </h3>
+                    <ul>
+                        <li> <a href="#">EXERCÍCIOS</a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                    </ul>
+                </div>
+                <div class="modulo laranja">
+                    <h3>MÓDULO 05 - </h3>
+                    <ul>
+                        <li> <a href="#">EXERCÍCIOS</a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                    </ul>
+                </div>
+                <div class="modulo verde-escuro">
+                    <h3>MÓDULO 06 - </h3>
+                    <ul>
+                        <li> <a href="#">EXERCÍCIOS</a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        <li> <a href="#"> ... </a> </li>
+                        
+                    </ul>
+                </div>
+                <!-- + módulos criados de acordo com o avanço dos estudos - alguns já com layout css -->
+            </nav>
+
+        </div>
+
+    </main>
+
+    <footer class="rodape"> 
+        <a href="https://github.com/ruan-lopes16" target="_blank"> <!-- colocando github para acessar -->
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg" width="40px"/>
+        </a>
+        <p>RUAN S. LOPES &copy; <?= date('Y');?></p> 
+<!-- código php - "M" mês-extenso, "m" mês-número, "D" dia da semana(inglês), "d" dia-número, "Y" ano-completo, "y" ano-dois último digitos -->
+        <a href="https://www.linkedin.com/in/ruanslopes/" target="_blank"> <!--colocado linkedim para acessar -->
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg" width="40px"/>
+        </a>
+    </footer>
+ 
+</body>
+</html> 
