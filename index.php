7e7c3cb295f504b93a1054d451505d52d67392bc
Ruan Lopes <121831380+rlopes16@users.noreply.github.com>
2024-06-30 21:42:26 -0300

    -adicionado ícone à aba do navegador;
    -rodapé atualizado c/ adição de LinkedIn, FUNÇÃO(<?= ... ?>) em php para ano atual e estilização para harmonia dos elementos;
    
    -nova pág .php para visualização de exercícios, e feito a estilização da mesma em css com botões de sem formação e para voltar à pág anterior s/ consultar histórico.

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
--+
 </head>
 <body>
     <header class="cabecalho">
